1f8ac1a0ccd02f353db45a8d56f517ba42156ce5
ShehlaEhet <shehla.speci@gmail.com>
2019-05-13 20:14:38 -0400

    pppp

--- Muhtar/src/package1/shehlabug.java
+++ Muhtar/src/package1/shehlabug.java
@@ -3,7 +3,7 @@ package package1;
 public class shehlabug {
 
 	public static void main(String[] args) {
-		// i wanna go to beach instead of studying in UEN
+		// i wanna go to beach instead of studying in UEN swswswsws
 
 	}
 
